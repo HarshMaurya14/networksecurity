c35e8b34f90ac49f444e9da5dc8efa83e02be714
unknown <m.harsh142005@gmail.com>
2026-06-16 22:32:39 +0530

    exception handling and login completed

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,20 @@
+import sys
+from networksecurity.logging import logger
+class NetworkSecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        self.error_message= error_message
+        __,_,exc_tb=error_details.exc_info()
+
+        self.lineno=exc_tb.tb_lineno
+        self.filename=exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return f"Error occured in python script name [{0}] at line number [{1}] error message [{2}]".format(self.filename,self.lineno,self.error_message)
+        self.filename,self.lineno,self.error_message
+
+
+
+
+                                                             
+
+      
--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -3,3 +3,15 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_paths=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_paths,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_paths,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+)
