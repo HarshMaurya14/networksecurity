2f4f83b01d07eb376fa1a804cd2c084227bbf1d4
unknown <m.harsh142005@gmail.com>
2026-06-18 22:18:41 +0530

    data validation completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -13,6 +13,8 @@ FILE_NAME = "phisingData.csv"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
 DATA_INGESTION_COLLECTION_NAME = "NetworkData"
 DATA_INGESTION_DATABASE_NAME = "KrishAI"
 
@@ -21,3 +23,11 @@ DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
 DATA_INGESTION_INGESTED_DIR = "ingested"
 
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO = 0.2
+
+## Data Validation related constants
+
+DATA_VALIDATION_DIR_NAME = "data_validation"
+DATA_VALIDATION_VALID_DIR = "validated"
+DATA_VALIDATION_INVALID_DIR = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "report.yaml"
