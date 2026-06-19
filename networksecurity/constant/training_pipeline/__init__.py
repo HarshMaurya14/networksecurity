312f3dbda903183a6eb3e325278cdd4b92d27495
unknown <m.harsh142005@gmail.com>
2026-06-19 15:40:05 +0530

    data transformation completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -31,6 +31,7 @@ DATA_VALIDATION_VALID_DIR = "validated"
 DATA_VALIDATION_INVALID_DIR = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 ## Data Transformation related constants
 DATA_TRANSFORMATION_DIR_NAME = "data_transformation"
@@ -40,8 +41,8 @@ DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR = "transformed_object"
 ##knn imputer to replace missing values in the dataset with the mean of the nearest neighbors. The parameters for the KNN imputer are defined in a dictionary called DATA_TRANSFORMATION_IMPUTER_PARAMS. The missing_values parameter is set to np.nan, which indicates that missing values are represented by NaN (Not a Number) in the dataset. The n_neighbors parameter is set to 3, which means that the imputer will consider the 3 nearest neighbors when calculating the mean to replace missing values. The weights parameter is set to "uniform", which means that all neighbors will be given equal weight when calculating the mean.
 
 DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
-    "missing_values": np.man,
-    "n_neighbours": 3,
+    "missing_values": np.nan,
+    "n_neighbors": 3,
     "weights": "uniform",
 
 }
