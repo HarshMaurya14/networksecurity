f7a8816a280b360c1b58fc076c821861c18c8bad
unknown <m.harsh142005@gmail.com>
2026-06-20 16:29:18 +0530

    pipeline completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -26,6 +26,9 @@ from sklearn.ensemble import (
 
 import mlflow
 
+import dagshub
+
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
@@ -43,7 +46,7 @@ class ModelTrainer:
             mlflow.log_metric("f1_score", f1_score)
             mlflow.log_metric("precision", precision_score)
             mlflow.log_metric("recall_score", recall_score)
-            mlflow.sklearn.log_model(best_model, "model")
+            mlflow.sklearn.log_model(best_model, name="model")
         
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
